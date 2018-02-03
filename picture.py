b9b5cb47238a91acf3488b48ea313b6db28feb21
zprimus <zprimus@mail.usf.edu>
2018-02-03 18:21:23 -0500

    clarifai as function

--- picture.py
+++ picture.py
@@ -4,12 +4,21 @@ from clarifai.rest import Image as C1Image
 
 key_item = 'af65160befb54398b247aa12dd1256e3'
 
-app = ClarifaiApp(api_key = key_item)
+img1 = 'https://prods3.imgix.net/images/articles/2017_04/Feature-restaurant-butcher-bakery-shops2.jpg'
 
-model = app.models.get("general-v1.3")
-img1 = C1Image(url = 'https://prods3.imgix.net/images/articles/2017_04/Feature-restaurant-butcher-bakery-shops2.jpg')
+def ProcessImage(imageLink):
 
-var = model.predict([img1])
-for i in range(0, 3):
-    #if(var["outputs"][0]["data"]["concepts"][i]["value"] > 0.97):
-    print(var["outputs"][0]["data"]["concepts"][i]["name"])
+    app = ClarifaiApp(api_key = key_item)
+
+    model = app.models.get("general-v1.3")
+    image = C1Image(url = imageLink)
+
+    var = model.predict([image])
+
+    for i in range(0, 3):
+        #if(var["outputs"][0]["data"]["concepts"][i]["value"] > 0.97):
+        print(var["outputs"][0]["data"]["concepts"][i]["name"])
+
+    return
+
+ProcessImage(img1)
